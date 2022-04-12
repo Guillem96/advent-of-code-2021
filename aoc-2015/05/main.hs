8efd163ed23c06ee8b1d8fd48386ff3aa6a726d6
Guillem96 <guillem.orellana@gmail.com>
2022-04-12 21:26:03 +0200

    Day 5

---
+++ aoc-2015/05/main.hs
@@ -0,0 +1,48 @@
+forbidden::[String]
+forbidden = ["ab", "cd", "pq", "xy"]
+
+count::Eq a => a -> [a] -> Int
+count s = length . filter (== s)
+
+contains3vowels::String -> Bool
+contains3vowels ss = sum [count v ss | v <- "aeiou"] >= 3
+
+repeatedLetter::String -> Bool
+repeatedLetter [x] = False
+repeatedLetter (x:y:xs) = x == y || repeatedLetter (y:xs)
+
+containsForbidden::String -> Bool
+containsForbidden [x] = False
+containsForbidden (x:y:xs) = elem ([x] ++ [y]) forbidden || containsForbidden (y:xs)
+
+isnice::String -> Bool
+isnice ss = not (containsForbidden ss) && contains3vowels ss && repeatedLetter ss
+
+chunks::String -> [String]
+chunks [x] = [[]]
+chunks [x, y] = [[x, y]]
+chunks (x:y:ns) = [[x, y]] ++ chunks nexts 
+  where 
+    nexts = if x == y && y == head ns then ns else (y:ns)
+
+containsRepeatedPair::String -> Bool
+containsRepeatedPair s = length [p | p <- chunked, count p chunked >= 2] > 1
+  where
+    chunked = chunks s
+
+containsSandwitch::String -> Bool
+containsSandwitch [_, _] = False
+containsSandwitch (x:y:z:ns) = x == z || containsSandwitch (y:z:ns)
+
+isnice'::String -> Bool
+isnice' ss = containsRepeatedPair ss && containsSandwitch ss
+
+part1::String -> String
+part1 = (++) "Part 1 - Number nice words: " . show . length . filter isnice . lines
+
+part2::String -> String
+part2 = (++) "Part 2 - Number nice words: " . show . length . filter isnice' . lines
+
+main = do
+    readFile "input.txt" >>= print . part1
+    readFile "input.txt" >>= print . part2
